df6453d44e4a091e79bf97c7e7415ef893571b03
Brayden Rieck <166241813+brieck1442@users.noreply.github.com>
2026-05-25 14:01:59 -0700

    Update CreateUser.ps1

--- CreateUser.ps1
+++ CreateUser.ps1
@@ -1,9 +1,11 @@
+Import-Module ActiveDirectory
+
 $FirstName = "John"
 $LastName = "Smith"
 $Username = "jsmith"
+
 $Password = ConvertTo-SecureString "P@ssw0rd123!" -AsPlainText -Force
 
-# CREATE USER
 New-ADUser `
     -Name "$FirstName $LastName" `
     -GivenName $FirstName `
@@ -12,4 +14,4 @@ New-ADUser `
     -UserPrincipalName "$Username@company.local" `
     -AccountPassword $Password `
     -Enabled $true `
-    -Path "OU=Finance,OU=Users,DC=company,DC=local"
+    -Path "OU=Finance,DC=company,DC=local"
